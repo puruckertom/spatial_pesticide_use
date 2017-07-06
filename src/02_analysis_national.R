14ddb3225451972edfa5db052db640300805215d
Tom Purucker <purucker.tom@gmail.com>
2017-07-06 00:52:23 -0400

    national

--- src/02_analysis_national.R
+++ src/02_analysis_national.R
@@ -23,3 +23,25 @@ for(compound in compounds){
 #rank negative values to go from highest to lowest
 compound_info[,4] <- rank(-compound_info[,2])
 View(compound_info)
+
+#top 20 1992, 2003, 2014
+#1992
+national_1992 <- national_by_year[,1:2]
+national_1992[,3] <- rank(-national_1992[,2])
+View(national_1992)
+top20 <- which(national_1992[,3]<=20)
+national_1992[top20,]
+
+#2003
+national_2003 <- national_by_year[,c(1,13)]
+national_2003[,3] <- rank(-national_2003[,2])
+View(national_2003)
+top20 <- which(national_2003[,3]<=20)
+national_2003[top20,]
+
+#2014
+national_2014 <- national_by_year[,c(1,24)]
+national_2014[,3] <- rank(-national_2014[,2])
+View(national_2014)
+top20 <- which(national_2014[,3]<=20)
+national_2014[top20,]
